b34a138a08f3bb501c2cd5a7063c395a2abc29d5
TaeHyeon <96982575+taebong98@users.noreply.github.com>
2023-02-10 11:44:41 +0900

    [level 0] Title: A로 B 만들기, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String before, String after) {
+        int answer = 0;
+        
+        
+        int beforeResult = 0;
+        for(int i = 0; i < before.length(); i++) {
+            int beforeNum = before.charAt(i) - 0;
+            beforeResult += beforeNum;
+        }
+        
+        int afterResult = 0;
+        for(int i = 0; i < after.length(); i++) {
+            int afterNum = after.charAt(i) - 0;
+            afterResult += afterNum;
+        }
+        
+        if (beforeResult == afterResult) {
+            return 1;
+        }
+        return 0;
+    }
+}

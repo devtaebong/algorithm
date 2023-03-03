4f05b5d2967f33bb2118a41867238a935f20056a
TaeHyeon <96982575+taebong98@users.noreply.github.com>
2023-03-03 13:32:18 +0900

    [Silver I] Title: NM과 K (1), Time: 1528 ms, Memory: 16284 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
@@ -1,46 +1,65 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
- static int[][] a = new int[10][10];
- static boolean[][] c = new boolean[10][10];
- static int n, m, k;
- static int ans = -2147483647;
- static int[] dx = {0,0,1,-1};
- static int[] dy = {1,-1,0,0};
- static void go(int cnt, int sum) {
- if (cnt == k) {
- if (ans < sum) ans = sum;
- return;
- }
- for (int x=0; x<n; x++) {
- for (int y=0; y<m; y++) {
- if (c[x][y]) continue;
- boolean ok = true;
- for (int i=0; i<4; i++) {
- int nx = x+dx[i];
- int ny = y+dy[i];
- if (0 <= nx && nx < n && 0 <= ny && ny < m) {
- if (c[nx][ny]) ok = false;
- }
- }
- if (ok) {
- c[x][y] = true;
- go(cnt+1, sum+a[x][y]);
- c[x][y] = false;
- }
- }
- }
- }
- public static void main(String args[]) {
- Scanner sc = new Scanner(System.in);
- n = sc.nextInt();
- m = sc.nextInt();
- k = sc.nextInt();
- for (int i=0; i<n; i++) {
- for (int j=0; j<m; j++) {
- a[i][j] = sc.nextInt();
- }
- }
- go(0, 0);
- System.out.println(ans);
- }
+    static int n, m, k;
+    static int[][] grid;
+    static boolean[][] visited;
+    static int answer = -2147483647;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        grid = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int x = Integer.parseInt(st.nextToken());
+                grid[i][j] = x;
+            }
+        }
+
+        go(0,0);
+        System.out.println(answer);
+    }
+
+    public static void go(int count, int sum) {
+        if (count == k) {
+            if (answer < sum) {
+                answer = sum;
+            }
+            return;
+        }
+
+        for (int x=0; x<n; x++) {
+            for (int y=0; y<m; y++) {
+                if (visited[x][y]) continue;
+                boolean ok = true;
+                for (int i=0; i<4; i++) {
+                    int nx = x+dx[i];
+                    int ny = y+dy[i];
+                    if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                        if (visited[nx][ny]) ok = false;
+                    }
+                }
+                if (ok) {
+                    visited[x][y] = true;
+                    go(count+1, sum+grid[x][y]);
+                    visited[x][y] = false;
+                }
+            }
+        }
+    }
 }
+

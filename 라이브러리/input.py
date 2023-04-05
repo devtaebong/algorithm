d4c147972bb1c2e4134b33b64249a64d3c4334ee
TaeHyeon <96982575+taebong98@users.noreply.github.com>
2023-04-05 14:01:17 +0900

    Update input.py

--- 라이브러리/input.py
+++ 라이브러리/input.py
@@ -5,5 +5,10 @@ sys.stdin=open("input.txt", "rt")
 # 입력받기
 n, m = map(int, input().split())
 
+# 2차원 배열 초기화 - 반복문
+matrix1 = []
+for i in range(n):
+  matrix.append(list(map(int, input())))
+
 # 2차원 배열 초기화 - 리스트 컴프리헨션
 matrix2 = [list(map(int, input().split())) for _ in range(m)]

a231dfe6c24efd02aa7990ac43ab5d6d3fc0fa1d
김태현 <96982575+taebong98@users.noreply.github.com>
2023-11-21 15:30:29 +0900

    [Silver III] Title: 두 수의 합, Time: 304 ms, Memory: 27516 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    /*
+    1. 아이디어
+    - a[i] + a[j] < x => count++
+    - bool[] -> 입력값 저장
+    - for -> bool[x - a[i]] == true => count += 1
+
+    2. 시간복잡도
+    O(N)
+
+    3. 자료구조
+    - int -> count
+    - bool[]
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+        int x = Integer.parseInt(br.readLine());
+
+        int a[] = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            a[i] = Integer.parseInt(arr[i]);
+        }
+
+        boolean[] exist = new boolean[1000001];
+        for (int i = 0; i < n; i++) {
+            exist[a[i]] = true;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int pairValue = x-a[i];
+            if (0 <= pairValue && pairValue <= 1000000) {
+                ans += exist[pairValue] ? 1 : 0;
+            }
+        }
+        System.out.println(ans/2);
+    }
+}

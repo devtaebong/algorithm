f4476be7f808e434d82a919b601d52d3ef7e804d
김태현 <96982575+taebong98@users.noreply.github.com>
2023-11-21 14:31:15 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1676 ms, Memory: 336848 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,50 +1,23 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        solution();
-    }
-
-        public static void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
 
-        int[] list = new int[n];
-        int max = 0;
         for (int i = 0; i < n; i++) {
-            list[i] = Integer.valueOf(br.readLine());
-
-            if (list[i] > max) max = list[i];
+            int num = Integer.parseInt(br.readLine());
+            arr[num]++;
         }
 
-        int[] counting = new int[max+1];
-        for (int i = 0; i < list.length; i++) {
-            int num = list[i];
-            counting[num]++;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < arr.length; i++) {
+            int num = arr[i];
+            while (num-- > 0) {
+                sb.append(i).append('\n');
+            }
         }
-        for (int i = 1; i < counting.length; i++) {
-            counting[i] += counting[i-1];
-        }
-
-
-        int[] answer = new int[list.length];
-        for (int i = list.length - 1; i >= 0; i--) {
-            int value = list[i];
-            int index = counting[value]--;
-
-            answer[index-1] = value;
-        }
-
-        for (int i : answer) {
-            sb.append(i).append("\n");
-        }
-
         System.out.println(sb);
     }
 }

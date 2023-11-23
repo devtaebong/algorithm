e1ca5911d9e9b13730b22332de385b3be36782d5
김태현 <96982575+taebong98@users.noreply.github.com>
2023-11-23 22:47:43 +0900

    [Silver IV] Title: 판화, Time: 136 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1730. 판화/판화.java
+++ 백준/Silver/1730. 판화/판화.java
@@ -26,25 +26,29 @@ public class Main {
         for (int i = 0; i < input.length(); i++) {
             char command = input.charAt(i);
             if (command == 'R') {
-                if (y == n -1) continue;
-                passHorizontal[x][y] = true;
-                passHorizontal[x][y+1] = true;
-                y++;
+                if (y+1 < n) {
+                    passHorizontal[x][y] = true;
+                    passHorizontal[x][y+1] = true;
+                    y++;
+                }
             } else if (command == 'D') {
-                if (x == n - 1) continue;
-                passVertical[x][y] = true;
-                passVertical[x+1][y] = true;
-                x++;
+                if (x+1 < n) {
+                    passVertical[x][y] = true;
+                    passVertical[x+1][y] = true;
+                    x++;
+                }
             } else if (command == 'L') {
-                if (y == 0) continue;
-                passHorizontal[x][y] = true;
-                passHorizontal[x][y-1] = true;
-                y--;
+                if (y-1 >= 0) {
+                    passHorizontal[x][y] = true;
+                    passHorizontal[x][y-1] = true;
+                    y--;
+                }
             } else if (command == 'U') {
-                if (x == 0) continue;
-                passVertical[x][y] = true;
-                passVertical[x-1][y] = true;
-                x--;
+                if (x-1 >= 0) {
+                    passVertical[x][y] = true;
+                    passVertical[x-1][y] = true;
+                    x--;
+                }
             }
         }
 

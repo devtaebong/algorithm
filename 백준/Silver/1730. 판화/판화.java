fddde6d39a20b20a2508a0303fec0b78c095bf92
김태현 <96982575+taebong98@users.noreply.github.com>
2023-11-23 22:43:41 +0900

    [Silver IV] Title: 판화, Time: 136 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/1730. 판화/판화.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+
+        // 입력
+        String[][] matrix = new String[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = ".";
+            }
+        }
+
+        boolean[][] passHorizontal = new boolean[n][n]; // 가로
+        boolean[][] passVertical = new boolean[n][n]; // 세로
+
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char command = input.charAt(i);
+            if (command == 'R') {
+                if (y == n -1) continue;
+                passHorizontal[x][y] = true;
+                passHorizontal[x][y+1] = true;
+                y++;
+            } else if (command == 'D') {
+                if (x == n - 1) continue;
+                passVertical[x][y] = true;
+                passVertical[x+1][y] = true;
+                x++;
+            } else if (command == 'L') {
+                if (y == 0) continue;
+                passHorizontal[x][y] = true;
+                passHorizontal[x][y-1] = true;
+                y--;
+            } else if (command == 'U') {
+                if (x == 0) continue;
+                passVertical[x][y] = true;
+                passVertical[x-1][y] = true;
+                x--;
+            }
+        }
+
+        String ans = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (passVertical[i][j] && passHorizontal[i][j]) {
+                    ans += "+";
+                } else if (passVertical[i][j] && !passHorizontal[i][j]) {
+                    ans += "|";
+                } else if (passHorizontal[i][j] && !passVertical[i][j]) {
+                    ans += "-";
+                } else {
+                    ans += ".";
+                }
+            }
+            ans += "\n";
+        }
+        System.out.println(ans);
+    }
+}

e3d3a87902d7cbfecad8c0220133ef6abb30209f
김태현 <96982575+taebong98@users.noreply.github.com>
2023-11-29 16:45:17 +0900

    [Silver V] Title: 단어 정렬, Time: 588 ms, Memory: 23728 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = br.readLine();
+        }
+
+        String[] distinctArr = Arrays.stream(str).distinct().toArray(String[]::new);
+
+        Arrays.sort(distinctArr, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                // 길이가 같다면 사전순
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+
+                // 길이가 짧은것 리턴
+                return o1.length() - o2.length();
+            }
+        });
+
+        for (int i = 0; i < distinctArr.length; i++) {
+            System.out.println(distinctArr[i]);
+        }
+    }
+}

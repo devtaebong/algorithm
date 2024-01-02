10288bc16b26f3a91a3bd3ef94daf512639453b7
김태현 <96982575+taebong98@users.noreply.github.com>
2024-01-02 11:41:14 +0900

    [Gold IV] Title: 빙산, Time: 512 ms, Memory: 120296 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,76 +1,100 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    static int n, m;
-    static int[][] earth;
-    static boolean[][] visited;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1 ,1};
-    static List<Ice> iceList = new ArrayList<>();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        earth = new int[n][m];
-        visited = new boolean[n][m];
-        iceList = new ArrayList<>();
+        int[] input = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                earth[i][j] = sc.nextInt();
-                if(earth[i][j] > 0) {
-                    iceList.add(new Ice(i, j, earth[i][j]));
+        int n = input[0];
+        int m = input[1];
+
+        // 빙산 입력
+        int[][] matrix = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            matrix[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        // 바다는 그래프탐색 범위에서 제외
+        // 빙산은 List로 관리
+        boolean[][] visited = new boolean[n][m];
+        List<Ice> iceList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] > 0) {
+                    iceList.add(new Ice(i, j, matrix[i][j]));
                 }
-                // iceList에 있는 빙산들의 좌표를 방문처리
                 visited[i][j] = true;
             }
         }
-        for(int year = 1; !iceList.isEmpty(); year++) {
-            // 빙산 녹이기
-            for(int i = 0; i < iceList.size(); i++) {
-                Ice ice = iceList.get(i);
-                for(int j = 0; j < 4; j++) {
-                    int nr = ice.row + dr[j];
-                    int nc = ice.col + dc[j];
-                    if(earth[nr][nc] == 0) ice.height--;
+
+        System.out.println(getYear(matrix, visited, iceList));
+    }
+
+    public static int getYear(int[][] matrix, boolean[][] visited, List<Ice> iceList) {
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, 1, -1};
+
+        for (int year = 1; !iceList.isEmpty(); year++) {
+            // iceList를 돌면서 matrix 바다 면적에 따라 빙산 높이를 조정
+            for (Ice ice : iceList) {
+                for (int i = 0; i < 4; i++) {
+                    int nr = dr[i] + ice.row;
+                    int nc = dc[i] + ice.col;
+
+                    if (matrix[nr][nc] == 0) ice.height--;
                 }
             }
 
-            // 빙산 녹은 후 높이 갱신
-            for(int i = 0; i < iceList.size(); i++) {
+            // matrix의 빙산을 녹인다 -> 루프를 따로 돌아야함
+            for (int i = 0; i < iceList.size(); i++) {
                 Ice ice = iceList.get(i);
-                if(ice.height <= 0) {
-                    earth[ice.row][ice.col] = 0;
-                    iceList.set(i, iceList.get(iceList.size()-1));
-                    iceList.remove(iceList.size()-1);
+                if (ice.height <= 0) {
+                    matrix[ice.row][ice.col] = 0;
+                    iceList.set(i, iceList.get(iceList.size() - 1));
+                    iceList.remove(iceList.size() - 1);
                     i--;
                 }
-                // DFS 탐색 대상 -> visited 초기화
                 else {
+                    matrix[ice.row][ice.col] = ice.height;
                     visited[ice.row][ice.col] = false;
                 }
             }
 
-            if(iceList.size() > 0 && dfs(iceList.get(0).row, iceList.get(0).col) != iceList.size()) {
-                System.out.println(year);
-                System.exit(0);
+            if (iceList.size() > 0 && dfs(matrix, visited, iceList.get(0).row, iceList.get(0).col) != iceList.size()) {
+                return year;
             }
         }
-        System.out.println(0);
+
+        return 0;
     }
-    static int dfs(int r, int c) {
+
+    public static int dfs(int[][] matrix, boolean[][] visited, int r, int c) {
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
         visited[r][c] = true;
-        int cnt = 1;
-        for(int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-            if(visited[nr][nc]) continue;
-            cnt += dfs(nr, nc);
+
+        int count = 1;
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
+
+            if (!visited[nr][nc] && matrix[nr][nc] > 0) {
+                count += dfs(matrix, visited, nr, nc);
+            }
         }
-        return cnt;
+
+        return count;
     }
 }
 
@@ -78,9 +102,19 @@ class Ice {
     int row;
     int col;
     int height;
-    public Ice(int r, int c, int h) {
-        this.row = r;
-        this.col = c;
-        this.height = h;
+
+    public Ice(int row, int col, int height) {
+        this.row = row;
+        this.col = col;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Ice{" +
+                "row=" + row +
+                ", col=" + col +
+                ", height=" + height +
+                '}';
     }
 }

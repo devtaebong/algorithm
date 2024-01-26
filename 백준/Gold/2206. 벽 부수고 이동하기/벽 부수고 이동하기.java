c403a8559b95f17330d5cafde73991b61d477eeb
김태현 <96982575+taebong98@users.noreply.github.com>
2024-01-26 16:26:21 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 824 ms, Memory: 146544 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -4,51 +4,72 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/*
+1. 아이디어
+- (1,1) => (n,m)으로 이동할 수 있는 최단거리 => BFS
+    - 이동 중에 벽을 하나 부실 수 있다. (안부숴도 됨)
+- 벽을 부수지 않고 탐색하는 경우
+- 벽을 부수고 탐색하는 경우
+
+2. 시간복잡도
+- O(NM)
+
+3. 자료구조
+class Point => int row, int col, int isBroken
+int[][][] visited => 방문 체크 배열
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] input = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-
         int n = input[0];
         int m = input[1];
+
         int[][] matrix = new int[n + 1][m + 1];
-        for (int i = 1; i < n + 1; i++) {
-            String s = br.readLine();
-            for (int j = 1; j < m + 1; j++) {
-                matrix[i][j] = s.charAt(j - 1) - '0';
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                char c = str.charAt(j);
+                matrix[i + 1][j + 1] = c - '0';
             }
         }
         int[][][] visited = new int[n + 1][m + 1][2];
 
         Queue<Point> q = new LinkedList<>();
-        visited[1][1][0] = 1;
         q.add(new Point(1, 1, 0));
+        visited[1][1][0] = 1;
 
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
+
         while (!q.isEmpty()) {
             Point now = q.poll();
-            if (now.r == n && now.c == m) {
-                System.out.println(visited[now.r][now.c][now.isBroken]);
+            if (now.row == n && now.col == m) {
+                System.out.println(visited[now.row][now.col][now.isBroken]);
                 return;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nr = now.r + dr[i];
-                int nc = now.c + dc[i];
-                if (nr <= 0 || nr > n || nc <= 0 || nc > m) {
+                int nr = dr[i] + now.row;
+                int nc = dc[i] + now.col;
+                // 탐색 범위 체크
+                if (nr < 1 || nr > n || nc < 1 || nc > m) {
                     continue;
                 }
 
+                // 방문하지 않은 경우
                 if (visited[nr][nc][now.isBroken] == 0) {
+                    // 다음 좌표의 벽을 부수지 않은 경우
                     if (matrix[nr][nc] == 0) {
-                        visited[nr][nc][now.isBroken] = visited[now.r][now.c][now.isBroken] + 1;
+                        visited[nr][nc][now.isBroken] = visited[now.row][now.col][now.isBroken] + 1;
                         q.add(new Point(nr, nc, now.isBroken));
                     }
+
+                    // 다음 좌표의 벽을 부수는 경우
                     else if (matrix[nr][nc] == 1 && now.isBroken == 0) {
-                        visited[nr][nc][1] = visited[now.r][now.c][0] + 1;
+                        visited[nr][nc][1] = visited[now.row][now.col][now.isBroken] + 1;
                         q.add(new Point(nr, nc, 1));
                     }
                 }
@@ -59,13 +80,13 @@ public class Main {
 }
 
 class Point {
-    int r;
-    int c;
+    int row;
+    int col;
     int isBroken;
 
-    public Point(int r, int c, int isBroken) {
-        this.r = r;
-        this.c = c;
+    public Point(int row, int col, int isBroken) {
+        this.row = row;
+        this.col = col;
         this.isBroken = isBroken;
     }
 }

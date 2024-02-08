8d91f041c17d0e08f75a6bff94318f162642b471
김태현 <96982575+taebong98@users.noreply.github.com>
2024-02-08 16:35:38 +0900

    [Gold V] Title: 동전 2, Time: 148 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -50,8 +50,14 @@ public class Main {
         // d[i]: i원을 만들수 있는 동전의 최솟값
         for (int i = 1; i < k + 1; i++) {
             for (int x : coin) {
-                if (i + x <= k) {
-                    dp[i + x] = Math.min(dp[i] + 1, dp[i + x]);
+                // 방법1: {i}값의 동전 개수를 알고 있을때
+//                if (i + x <= k) {
+//                    dp[i + x] = Math.min(dp[i] + 1, dp[i + x]);
+//                }
+
+                // 방법2: {i}값을 만들고 싶을 때
+                if (i - x >= 1) {
+                    dp[i] = Math.min(dp[i - x] + 1, dp[i]);
                 }
             }
         }

0eeeb5f969575086a64fe9b43662411f91417177
김태현 (Tae Hyeon, Kim) <96982575+taebong-k@users.noreply.github.com>
2024-06-23 19:29:26 +0900

    [Silver III] Title: N과 M (1), Time: 264 ms, Memory: 23344 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,60 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    static int n = 0;
-    static int m = 0;
-    static boolean[] ch;
-    static int[] a;
     static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int M;
+    static int[] selected;
+    static boolean[] check;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        input();
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        ch = new boolean[n+1];
-        a = new int[n];
-        DFS(0);
+        // N까지 자연수 중 중복없이 M개의 수를 고른다.
+        recursive(1);
+
         System.out.println(sb);
     }
 
-    private static void DFS(int idx) {
-        if (m == idx) {
-            // 출력
-            for (int i = 0; i < m; i++) {
-                sb.append(a[i] + " ");
+    // k ~ N 까지 조합을 선택한다.
+    public static void recursive(int k) {
+        // 조건을 만족하는 경우
+        if (k == M + 1) {
+            for (int i = 1; i < selected.length; i++) {
+                sb.append(selected[i]).append(' ');
             }
             sb.append("\n");
-            return;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if(ch[i]) continue;
+        // 조건에 해당하는 경우를 찾지 못한경우
+        else {
+            for (int candidate = 1; candidate <= N; candidate++) {
+                if (check[candidate]) {
+                    continue;
+                }
 
-            ch[i] = true;
-            a[idx] = i;
-            DFS(idx+1);
-            ch[i] = false;
+                check[candidate] = true;
+                selected[k] = candidate;
+                recursive(k + 1);
+                check[candidate] = false;
+                selected[k] = 0;
+            }
         }
+    }
+
+    public static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] nm = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        N = nm[0];
+        M = nm[1];
+
+        selected = new int[M + 1];
+        check = new boolean[N + 1];
     }
 }
